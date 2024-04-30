227e005d98e7b2a7787b5a9f9a34eea765aba817
Luis Sarabia <luissarabia80@gmail.com>
2024-04-30 19:15:41 -0400

    [update] Exception Rpc

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -36,8 +32,8 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     const lastPage = Math.ceil(totalPage / limit);
 
     const data = await this.product.findMany({
-      take: (page - 1) * limit,
-      skip: page,
+      take: limit,
+      skip: (page - 1) * limit,
       where: {
         available: true,
       },
@@ -61,7 +57,11 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       },
     });
 
-    if (!product) throw new NotFoundException();
+    if (!product)
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
 
     return product;
   }
